e8d00a2289f365afb3a167f9d907c4e0a5e6e9b7
joaoPatriarca <joaomarcospatriarca@outlook.com>
2024-10-02 21:33:11 -0300

    Metodos

---
+++ CalculoDeEnergia/Calculos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoDeEnergia
+{
+    public class Calculos
+    {
+        public double Kw { get; set; }
+        public double Talao { get; set; }
+        public double Consumo { get; set; }
+        public double UltimaLeituraEnergia { get; set; }
+        public double LeituraAtualEnergia { get; set; }
+        public double UltimaleituraAgua { get; set; }
+        public double LeituraAtualAgua { get; set; }
+
+        public double Ckw()
+        {
+            return Consumo / Talao;
+        }
+        
+        public double ConsumoTotal()
+        {
+            double ConsumoEnergia = LeituraAtualEnergia - UltimaLeituraEnergia;
+            double ConsumoAgua = LeituraAtualAgua - UltimaleituraAgua;
+            return ConsumoEnergia + ConsumoAgua;
+
+        }
+
+        public double ValorTotal()
+        {
+            return ConsumoTotal() * Kw;
+        }
+    }
+}
